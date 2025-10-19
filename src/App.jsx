a2fbb9bedbfed175fa0fdad3d41e7ba6a8f65cfe
poojaa09 <poojarawat0329@gmail.com>
2025-10-19 17:09:02 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import Navbar from "./components/Navbar.jsx";
-const App=()=>{
+import NavBar from "./components/NavBar.jsx";
+import Hero from "./components/Hero.jsx";
+import ProductViewer from "./components/ProductViewer.jsx";
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/all";
+import Showcase from "./components/Showcase.jsx";
+import Performance from "./components/Performance.jsx";
+import Features from "./components/Features.jsx";
+import Highlights from "./components/Highlights.jsx";
+import Footer from "./components/Footer.jsx";
+
+gsap.registerPlugin(ScrollTrigger)
+
+const App = () => {
     return (
         <main>
-            <Navbar />
-
+            <NavBar />
+            <Hero />
+            <ProductViewer />
+            <Showcase />
+            <Performance />
+            <Features />
+            <Highlights />
+            <Footer />
         </main>
     )
 }
-export default App;
+
+export default App
